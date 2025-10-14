e34a9438955093c2abe907fa750b54c976e0205f
tasarien <tasarien@gmail.com>
2025-10-14 14:04:18 +0200

    anchor-point card base

--- lib/presentations/widgets/from models/anchor_point_widget.dart
+++ lib/presentations/widgets/from models/anchor_point_widget.dart
@@ -12,8 +12,69 @@ class AnchorPointWidget extends StatefulWidget {
 class _AnchorPointWidgetState extends State<AnchorPointWidget> {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      widget.anchorPoint.name ?? "No name"
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Center(
+      child: Column(
+        spacing: 20,
+        children: [
+          Container(
+            width: 300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Card(
+                    child: Container(
+                      width: 270,
+                      height: 270,
+                    ),
+                  ),
+                ),
+                ClipOval(
+                  child: Image.asset(
+                    'assets/images/auth_gate.png', // <-- Replace with your image asset
+                    width: 280,
+                    height: 280,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                 Transform.translate(
+                  offset: Offset(0, 100),
+                   child: Container(
+                        width: 220,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: colorScheme.surface,
+                          border: Border.all(color: colorScheme.tertiary),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Text(
+                          widget.anchorPoint.name!,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                 ),
+              ],
+            ),
+          ),
+          Container(
+            width: 300,
+            
+            child: Card(child: Text("text"),),
+          )
+        ],
+      ),
     );
   }
+   
+  
 }
